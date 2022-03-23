4dc300791fd32cc8d48d001e7bed06bc1ace50fd
MORIS CACERES <85360281+MORIS-CACERES@users.noreply.github.com>
2022-03-23 16:26:58 -0600

    estoy haciendo un cambio de Visual

--- Registro_Familiar/Registro_Familiar/Form1.Designer.cs
+++ Registro_Familiar/Registro_Familiar/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(234, 39);
+            this.textBox1.Location = new System.Drawing.Point(199, 163);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(307, 20);
             this.textBox1.TabIndex = 0;
