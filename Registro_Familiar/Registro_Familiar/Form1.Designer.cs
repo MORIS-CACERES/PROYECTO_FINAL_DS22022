8cd1a60bf5c467914a9a1172365c0e23a67e5188
MORIS CACERES <85360281+MORIS-CACERES@users.noreply.github.com>
2022-03-23 16:11:40 -0600

    ESTO ES UNA PRUEVA CONFIRMACION DESDE VISUAL

--- Registro_Familiar/Registro_Familiar/Form1.Designer.cs
+++ Registro_Familiar/Registro_Familiar/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(343, 154);
+            this.textBox1.Location = new System.Drawing.Point(375, 107);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
